c14dfa4086cb4e556446f00a2606b17371bb855d
Tubocass-PC\IO Dev <tubocass@gmail.com>
2017-12-01 20:54:31 -0600

    Smoking Scene Initial

---
+++ Assets/Scripts/HitBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitBar : MonoBehaviour 
+{
+	public GameObject bar;
+	bool isMouseover;
+	Image image;
+
+	void Awake()
+	{
+		image = GetComponent<Image>();
+	}
+
+	public void Update()
+	{
+		if(isMouseover && Input.GetMouseButton(0))
+		bar.transform.localPosition += Vector3.right;
+	}
+	public void MouseOver()
+	{
+		isMouseover = true;
+	}
+
+}
