c98dddaa0019dcdcba62fb40fbc00f35980eec29
J Doh <jdohgamer@Gmail.com>
2017-12-02 15:34:04 -0600

    MOAR UI. Final Roll Action Implemented. Preparing to Merge to gain HitBar Functionality.

--- Assets/Scripts/CannabisDrag.cs
+++ Assets/Scripts/CannabisDrag.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CannabisDrag : MonoBehaviour {
 
 	public GameObject CannaPrefab;
+	public GameObject PapePrefab;
 	private GameObject Cannabis;
 	public int cannaCount;
 	private Vector3 mousepos;
@@ -12,51 +13,45 @@ public class CannabisDrag : MonoBehaviour {
 	public Vector3 screenPosition;
 	public Vector3 offset;
 	public GameObject target;
+	public Transform chicken;
+	Vector3 offSet;
+
 
 
 	// Use this for initialization
 	void Start () {
+		
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
+		
 		//Debug.Log (cannaCount);
 
 		//Create code to only create cannabis pinch object when mouse is within range of Grinder
 
-			
-
-
-
 		if (Input.GetMouseButtonDown (0)) {
+			if (!GameObject.FindWithTag ("Pinch")) {
+				
 			mousepos = Input.mousePosition;
 			mousepos.z = 900;
 			mousepos = Camera.main.ScreenToWorldPoint (mousepos);
 
-			if (cannaCount < 1) {
-			
+				if (GameObject.FindWithTag ("Blunt") || GameObject.FindWithTag ("Joint") ) {
+					//if (mousepos == GameObject.Find ("Pinch").GetComponent<SpriteRenderer> ().bounds.center) {
 
-				if (!GameObject.Find ("RollingBPaper")) {
+						Debug.Log ("Create pinch of weed");
+						Cannabis = Instantiate (CannaPrefab, mousepos, Quaternion.identity) as GameObject;
+					//}
 
-					Debug.Log ("Create pinch of weed");
-					Cannabis = Instantiate (CannaPrefab, mousepos, Quaternion.identity) as GameObject;
-				}
-				cannaCount++;
 				} else {
-				Debug.Log ("You already have enough");
-
-			}
-
-		}
-
+				Debug.Log ("Where's the papes?");
+				}
+			} else {
+				Debug.Log ("Already got enough bruh");
+				}
+				}
 	}
 }
-
-
-
-	
-
-
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -7,7 +7,12 @@ public class UI : MonoBehaviour {
 
 	public GameObject RollingBpaper;
 	public GameObject RollingJpaper;
+	public GameObject RollActionButton;
+	public GameObject HintText;
+	public GameObject Pinch;
 	private bool isPaperActive;
+	private bool isRollButtonActive = false;
+
 
 
 	public void CreateBPaper () {
@@ -16,11 +21,21 @@ public class UI : MonoBehaviour {
 		if (!isPaperActive) {
 			Instantiate (RollingBpaper);
 			GameObject.Find ("Button B").GetComponentInChildren<Text> ().text = "Put Away";
+
+			GameObject.Find ("Button J").GetComponentInChildren<Text> ().text = "";
+			GameObject Htext = Instantiate (HintText, new Vector3(150,70,0), Quaternion.identity);
+			Htext.transform.SetParent (GameObject.FindWithTag ("Canvas").transform, false);
+
+			Debug.Log ("created HintText");
 			isPaperActive = true;
 		} else {
 			Debug.Log("Putting Away");
-			Destroy (GameObject.FindWithTag ("Blunt"));
+			DestroyImmediate (GameObject.FindWithTag ("Blunt"));
+			DestroyImmediate (GameObject.FindWithTag ("Pinch"));
+			DestroyImmediate (GameObject.FindWithTag ("HintText"));
 			GameObject.Find ("Button B").GetComponentInChildren<Text> ().text = "Prepare Blunt Paper";
+			GameObject.Find ("Button J").GetComponentInChildren<Text> ().text = "Prepare Joint Paper";
+
 			isPaperActive = false;
 			}
 	}
@@ -30,25 +45,57 @@ public class UI : MonoBehaviour {
 		if (!isPaperActive) {
 			Instantiate (RollingJpaper);
 			GameObject.Find ("Button J").GetComponentInChildren<Text> ().text = "Put Away";
-			isPaperActive = true;
+			GameObject.Find ("Button B").GetComponentInChildren<Text> ().text = "";
 			Debug.Log (isPaperActive);
+			Instantiate (HintText);
+			isPaperActive = true;
 
 		} else {
 				Debug.Log ("Putting Away");
-				Destroy (GameObject.FindWithTag ("Joint"));
-				isPaperActive = false;
+				DestroyImmediate (GameObject.FindWithTag ("Joint"));
+				DestroyImmediate (GameObject.FindWithTag ("Pinch"));
+			DestroyImmediate (GameObject.FindWithTag ("HintText"));
 				GameObject.Find ("Button J").GetComponentInChildren<Text> ().text = "Prepare Joint Paper";
+				GameObject.Find ("Button B").GetComponentInChildren<Text> ().text = "Prepare Blunt Paper";
+				isPaperActive = false;
+
 			}
 		}
 
+	public void RollAction () {
+
+		Debug.Log ("Destroy UI");
+		DestroyImmediate (GameObject.FindWithTag ("Canvas"));
+
+		DestroyImmediate (GameObject.FindWithTag ("Strain"));
+		DestroyImmediate (GameObject.FindWithTag ("HintText"));
+		DestroyImmediate (GameObject.FindWithTag ("UI"));
+
+
+
+
+
+
+
+	}
+
+
+
 	// Use this for initialization
 	void Start () {
-		
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (isPaperActive && GameObject.FindWithTag ("Pinch") && !isRollButtonActive) {
+			GameObject.Find ("StartRollActionButton").SetActive (true);
+			isRollButtonActive = true;
+
+		} else {
+			
 		
 	}
+}
 }
